2448fad1645198d006a687945d132abbe25e9210
Radmila <157131850+RadmilaGram@users.noreply.github.com>
2025-11-13 22:13:05 +0200

    Deleted app.module.d.ts

--- dist/stats/stats.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ProductsService } from '../products/products.service';
-import { ProductStatsResponseDto } from './dto/product-stats-response.dto';
-export declare class StatsController {
-    private readonly productsService;
-    constructor(productsService: ProductsService);
-    getProductStats(): ProductStatsResponseDto;
-}
